e6806b02608992c5f00b9ddc0de6fc59d96cfbf7
Niño James Tan <njtan142@gmail.com>
2021-02-15 17:20:18 +0800

    reformat

--- camera.py
+++ camera.py
@@ -5,24 +5,17 @@ class Camera:
         self.follow = follow
         self.obj = self_object
 
-    def update(self, list, screen, follow=None):
+    def update(self, object_list, screen, follow=None):
         if follow is None:
             follow = self.follow
         self.follow = follow
-        horizontal_difference = self.obj.x - self.follow.x 
+        horizontal_difference = self.obj.x - self.follow.x
         vertical_difference = self.obj.y - self.follow.y
-        x = (horizontal_difference)
-        y = (vertical_difference)
+        x = horizontal_difference
+        y = vertical_difference
         print(x, y)
         for obj in self.objects:
-            if obj.x > 0 - obj.image.get_width() and obj.x < screen.get_width():
-                if obj.y > 0 - obj.image.get_height() and obj.y < screen.get_height():
-                    obj.update(screen)
-            obj.move(x, y, list)
-
-
-
-
-
-
-
+            if 0 - obj.image.get_width() < obj.x < screen.get_width()\
+                    and 0 - obj.image.get_height() < obj.y < screen.get_height():
+                obj.update(screen)
+            obj.move(x, y, object_list)
